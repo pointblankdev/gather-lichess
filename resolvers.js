261431fe247ef320143059f1e4a3f19076542830
Al-Kassim Kassim <alkassimk@gmail.com>
2021-05-21 15:39:21 +0100

    🚀 generate schema & some resolvers

--- resolvers.js
+++ resolvers.js
@@ -1,12 +1,59 @@
+const axios = require('axios');
+const baseUrl = `https://lichess.org`;
+
 const resolvers = {
   Query: {
-    listPosts: async () => [{ id: "1" }],
-    getPost: async (_, args) => ({ id: "1" }),
-  },
-  Mutation: {
-    createPost: (_, args) => ({ id: "1" }),
-    updatePost: (_, args) => ({ id: "1" }),
-    deletePost: (_, args) => ({ id: "1" }),
+    getUser: async (root, args) => {
+      const { data } = await axios.get(baseUrl + `/api/user/${args.username}`);
+
+      return data;
+    },
+
+    getUsersStatus: async (root, args) => {
+      const { data } = await axios.get(
+        baseUrl + `/api/users/status?ids=${args.ids}`
+      );
+
+      return data;
+    },
+
+    getUserRatingHistory: async (root, args) => {
+      const { data } = await axios.get(
+        baseUrl + `/api/user/${args.username}/rating-history`
+      );
+
+      return data;
+    },
+
+    getUserActivity: async (root, args) => {
+      const { data } = await axios.get(
+        baseUrl + `/api/user/${args.username}/activity`
+      );
+
+      return data;
+    },
+
+    getLiveStreamers: async () => {
+      const { data } = await axios.get(baseUrl + '/streamer/live');
+      return data;
+    },
+
+    getCrosstable: async (root, args) => {
+      let requestUrl;
+      if (args.matchup) {
+        requestUrl =
+          baseUrl + `/api/crosstable/${args.user1}/${args.user2}?matchup=true`;
+      } else {
+        requestUrl = baseUrl + `/api/crosstable/${args.user1}/${args.user2}`;
+      }
+
+      console.log(requestUrl);
+      const { data } = await axios.get(requestUrl);
+
+      console.log(data);
+
+      return data;
+    },
   },
 };
 
--- typeDefs.js
+++ typeDefs.js
@@ -1,21 +1,276 @@
-const { gql } = require("apollo-server-lambda");
+const { gql } = require('apollo-server-lambda');
 
 const typeDefs = gql`
-  # Models
-  type Post {
-    id: String
+  schema {
+    query: Query
   }
+  scalar JSON
+    @specifiedBy(
+      url: "http://www.ecma-international.org/publications/files/ECMA-ST/ECMA-404.pdf"
+    )
 
-  # Operations
   type Query {
-    listPosts: [Post]
-    getPost(id: ID): Post
+    getUserActivity(limit: Int, username: String!): [UserActivityListItem]
+
+    getUser(username: String!): User
+
+    getUserRatingHistory(username: String!): [RatingHistory]
+
+    getUsersStatus(
+      ids: String!
+
+      limit: Int
+    ): [ApiUsersStatusListItem]
+
+    getLiveStreamers(limit: Int): [StreamerLiveList]
+
+    getCrosstable(matchup: Boolean, user1: String!, user2: String!): Crosstable!
   }
 
-  type Mutation {
-    createPost(id: ID): Post
-    updatePost(id: ID): Post
-    deletePost(id: ID): Post
+  type UserActivityListItem {
+    correspondenceEnds: JSON
+    correspondenceMoves: JSON
+    follows: JSON
+    games: JSON
+    interval: JSON
+    posts: JSON
+    practice: [PracticeListItem]
+    puzzles: Puzzles
+    teams: [TeamsListItem]
+    tournaments: JSON
+  }
+
+  type PracticeListItem {
+    name: String
+    nbPositions: Int
+    url: String
+  }
+
+  type Puzzles {
+    score: TheScoreSchema!
+  }
+
+  type TheScoreSchema {
+    draw: Int!
+    loss: Int!
+    rp: Rp!
+    win: Int!
+  }
+
+  type Rp {
+    after: Int!
+    before: Int!
+  }
+
+  type TeamsListItem {
+    name: String
+    url: String
+  }
+
+  type User {
+    blocking: Boolean!
+    completionRate: Int!
+    count: Count!
+    createdAt: Int!
+    disabled: Boolean
+    followable: Boolean!
+    following: Boolean!
+    followsYou: Boolean!
+    id: String!
+    language: String
+    nbFollowers: Int!
+    nbFollowing: Int!
+    online: Boolean!
+    patron: Boolean!
+    perfs: Perfs!
+    playTime: PlayTime!
+    playing: String!
+    profile: Profile!
+    seenAt: Int!
+    streaming: Boolean!
+    title: String!
+    tosViolation: Boolean!
+    url: String!
+    username: String!
+  }
+
+  type Count {
+    ai: Int!
+    all: Int!
+    bookmark: Int!
+    draw: Int!
+    drawH: Int!
+    import: Int!
+    loss: Int!
+    lossH: Int!
+    me: Int!
+    playing: Int!
+    rated: Int!
+    win: Int!
+    winH: Int!
+  }
+
+  type Perfs {
+    atomic: Atomic!
+    blitz: Blitz!
+    bullet: Bullet!
+    chess960: Chess960!
+    classical: Classical!
+    correspondence: Correspondence!
+    horde: Horde!
+    kingOfTheHill: KingOfTheHill!
+    puzzle: Puzzle!
+    racingKings: RacingKings!
+    rapid: Rapid!
+    storm: Storm!
+    ultraBullet: UltraBullet!
+  }
+
+  type Atomic {
+    games: Int!
+    prog: Int!
+    prov: Boolean!
+    rating: Int!
+    rd: Int!
+  }
+
+  type Blitz {
+    games: Int!
+    prog: Int!
+    prov: Boolean!
+    rating: Int!
+    rd: Int!
+  }
+
+  type Bullet {
+    games: Int!
+    prog: Int!
+    prov: Boolean!
+    rating: Int!
+    rd: Int!
+  }
+
+  type Chess960 {
+    games: Int!
+    prog: Int!
+    prov: Boolean!
+    rating: Int!
+    rd: Int!
+  }
+
+  type Classical {
+    games: Int!
+    prog: Int!
+    prov: Boolean!
+    rating: Int!
+    rd: Int!
+  }
+
+  type Correspondence {
+    games: Int!
+    prog: Int!
+    prov: Boolean!
+    rating: Int!
+    rd: Int!
+  }
+
+  type Horde {
+    games: Int!
+    prog: Int!
+    prov: Boolean!
+    rating: Int!
+    rd: Int!
+  }
+
+  type KingOfTheHill {
+    games: Int!
+    prog: Int!
+    prov: Boolean!
+    rating: Int!
+    rd: Int!
+  }
+
+  type Puzzle {
+    games: Int!
+    prog: Int!
+    prov: Boolean!
+    rating: Int!
+    rd: Int!
+  }
+
+  type RacingKings {
+    games: Int!
+    prog: Int!
+    prov: Boolean!
+    rating: Int!
+    rd: Int!
+  }
+
+  type Rapid {
+    games: Int!
+    prog: Int!
+    prov: Boolean!
+    rating: Int!
+    rd: Int!
+  }
+
+  type Storm {
+    runs: Int!
+    score: Int!
+  }
+
+  type UltraBullet {
+    games: Int!
+    prog: Int!
+    prov: Boolean!
+    rating: Int!
+    rd: Int!
+  }
+
+  type PlayTime {
+    total: Int!
+    tv: Int!
+  }
+
+  type Profile {
+    bio: String!
+    country: String!
+    ecfRating: Int!
+    fideRating: Int!
+    firstName: String!
+    lastName: String!
+    links: String!
+    location: String!
+    uscfRating: Int!
+  }
+
+  type ApiUsersStatusListItem {
+    id: String
+    name: String
+    online: Boolean
+    patron: Boolean
+    playing: Boolean
+    streaming: Boolean
+    title: String
+  }
+
+  type RatingHistory {
+    name: String
+    points: [JSON]
+  }
+
+  type StreamerLiveList {
+    id: String
+    name: String
+    online: Boolean
+    patron: Boolean
+    title: String
+  }
+
+  type Crosstable {
+    users: JSON
+    matchup: JSON
+    nbGames: Int
   }
 `;
 
